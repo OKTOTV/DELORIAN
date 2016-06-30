2b4bd71e805deef22c2ccca576288e0f94a7fded
rs <schmidt91robin@icloud.com>
2016-06-30 15:24:13 +0200

    defined playlist

---
+++ src/Okto/MediaBundle/Entity/Playlist.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Okto\MediaBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Oktolab\MediaBundle\Entity\Playlist as BasePlaylist;
+
+/**
+ * Playlist
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Playlist extends BasePlaylist
+{
+}
