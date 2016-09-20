08a8b898c0035c4332166c6ed40faed6fe65af35
rs <schmidt91robin@icloud.com>
2016-09-20 14:31:53 +0200

    fixed use statement

--- src/AppBundle/Form/SeriesImportProgressType.php
+++ src/AppBundle/Form/SeriesImportProgressType.php
@@ -6,7 +6,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Okto\MediaBundle\Entity\Series;
+use AppBundle\Entity\Series;
 
 class SeriesImportProgressType extends AbstractType
 {
