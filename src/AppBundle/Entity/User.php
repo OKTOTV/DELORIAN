4ea68b9204a14ba28d3642a687844ee4d74a4711
rs <schmidt91robin@icloud.com>
2017-10-11 13:33:02 +0200

    updated userclass and target entity

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -9,7 +9,7 @@ use Bprs\UserBundle\Entity\User as BprsUser;
  * User
  *
  * @ORM\Table()
-* @ORM\Entity(repositoryClass="Bprs\UserBundle\Entity\UserRepository")
+ * @ORM\Entity(repositoryClass="Bprs\UserBundle\Entity\UserRepository")
  */
 class User extends BprsUser
 {
