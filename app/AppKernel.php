8ed206a594dfc97243b47cb35f77681327110cbd
rs <schmidt91robin@icloud.com>
2018-05-30 15:20:51 +0200

    started to make DELORIAN fit for Symfony3

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -25,7 +25,6 @@ class AppKernel extends Kernel
             new Oneup\UploaderBundle\OneupUploaderBundle(),
             new Knp\Bundle\GaufretteBundle\KnpGaufretteBundle(),
             new Bprs\CommandLineBundle\BprsCommandLineBundle(),
-            //new Okto\MediaBundle\MediaBundle(),
             new Okto\MediaBundle\OktoMediaBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new AppBundle\AppBundle(),
@@ -48,5 +47,20 @@ class AppKernel extends Kernel
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
         $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
+    }
+
+    public function getRootDir()
+    {
+        return __DIR__;
+    }
+
+    public function getCacheDir()
+    {
+        return dirname(__DIR__).'/var/cache/'.$this->environment;
+    }
+
+    public function getLogDir()
+    {
+        return dirname(__DIR__).'/var/logs';
     }
 }
