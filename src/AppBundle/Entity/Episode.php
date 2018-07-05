73d014d1ee5584bc2b206783ef1d9e577d1d5889
rs <schmidt91robin@icloud.com>
2018-07-05 08:35:32 +0200

    updated exclusion policy on episode for jms

--- src/AppBundle/Entity/Episode.php
+++ src/AppBundle/Entity/Episode.php
@@ -3,9 +3,12 @@
 namespace AppBundle\Entity;
 use Doctrine\ORM\Mapping as ORM;
 use Okto\MediaBundle\Entity\Episode as OktoEpisode;
+use JMS\Serializer\Annotation as JMS;
 
 /**
  * @ORM\Entity(repositoryClass="Okto\MediaBundle\Entity\Repository\EpisodeRepository")
+ * @JMS\AccessType("public_method")
+ * @JMS\ExclusionPolicy("all")
  */
 class Episode extends OktoEpisode {
 
