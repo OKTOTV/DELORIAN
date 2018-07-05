4a4d5dfc002e98665278961bd211f80939549f2b
rs <schmidt91robin@icloud.com>
2018-07-05 20:02:44 +0200

    added exclusion policy for tags

--- src/AppBundle/Entity/Tag.php
+++ src/AppBundle/Entity/Tag.php
@@ -3,12 +3,13 @@
 namespace AppBundle\Entity;
 use Doctrine\ORM\Mapping as ORM;
 use Okto\MediaBundle\Entity\Tag as OktoTag;
+use JMS\Serializer\Annotation as JMS;
 
 /**
  * @ORM\Entity(repositoryClass="Okto\MediaBundle\Entity\Repository\TagRepository")
+ * @JMS\AccessType("public_method")
+ * @JMS\ExclusionPolicy("all")
  */
 class Tag extends OktoTag {
 
 }
-
- ?>
